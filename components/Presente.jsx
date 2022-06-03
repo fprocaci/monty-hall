cb2a0cb2e6c113bab1d129a3fd7036cb39fdf71e
Fabricio Procaci <fprocaci@gmail.com>
2022-06-03 16:11:34 -0300

    criando presente e porta

--- components/Presente.jsx
+++ components/Presente.jsx
@@ -1,9 +1,12 @@
-import style from '../src/styles/Presente.module.css'
+import styles from '../src/styles/Presente.module.css'
 
 export default function Presente(){
     return(
-        <div className={style.presente}>
-
+        <div className={styles.presente}>
+            <div className={styles.tampa}></div>
+            <div className={styles.corpo}></div>
+            <div className={styles.laco1}></div>
+            <div className={styles.laco2}></div>
         </div>
     )
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,12 @@
+import Porta from "../../components/Porta";
+import Presente from "../../components/Presente";
 
 
 export default function Home() {
   return (
     <div>
-      
+      <Presente />
+      <Porta />
     </div>
   )
 }
